c62bf8cca4cb0c722a8133690a2ee8b68f7a8f24
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2025-07-02 18:14:04 +0530

    update initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.listen(8080, () => {
+  try {
+    console.log("connection successfully");
+  } catch (error) {
+    console.log("error in connection", error);
+  }
+});
