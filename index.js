ba99b45fcaef9d010a32d0251323d881d8f91465
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2025-07-03 20:55:53 +0530

    update different env

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const { router } = require("./src/router/router");
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
 
 const app = express();
 app.use(express.json());
 
-app.use("/",router)
+app.use("/", router);
 
 app.use(() => {
   throw new Error("request url is not found.");
 });
 
-app.listen(8080, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   try {
-    console.log("connection successfully");
+    console.log(
+      `Server listening on port ${PORT} in ${process.env.NODE_ENV} mode`
+    );
   } catch (error) {
     console.log("error in connection", error);
   }
 });
-
